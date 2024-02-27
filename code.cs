6a3fa8d460b3a9c42703955f32c5817b6efb00c6
Marcus Live <161459351+livematel@users.noreply.github.com>
2024-02-27 13:17:08 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,29 @@
+    public int UpdateOrder
+        {
+            get { return _updateOrder; }
+            set
+            {
+                if (_updateOrder != value)
+                {
+                    _updateOrder = value;
+                    OnUpdateOrderChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+        protected override void InsertItem(int index, IGameComponent item)
+        {
+            if (base.IndexOf(item) != -1)
+            {
+                throw new ArgumentException("Cannot Add Same Component Multiple Times");
+            }
+            base.InsertItem(index, item);
+            if (item != null)
+            {
+                this.OnComponentAdded(new GameComponentCollectionEventArgs(item));
+            }
+        }	protected GamePlatform(Game game)
+        {
+            if (game == null)
+                throw new ArgumentNullException("game");
+            Game = game;
+        }
